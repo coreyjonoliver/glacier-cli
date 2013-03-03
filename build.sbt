432da90f28cfb2230558043706ead2b8346638a0
Corey Oliver <corey.jon.oliver@gmail.com>
2013-03-03 00:16:52 -0600

    Some experimentation with scallop

--- build.sbt
+++ build.sbt
@@ -8,8 +8,13 @@ scalaVersion := "2.9.2"
 
 libraryDependencies ++= Seq(
   "com.amazonaws" % "aws-java-sdk" % "1.3.32",
+  "com.github.scopt" %% "scopt" % "2.1.0",
   "org.scalatest" %% "scalatest" % "1.8" % "test",
   "org.rogach" %% "scallop" % "0.7.0"
+)
+
+resolvers ++= Seq(
+  "sonatype-public" at "https://oss.sonatype.org/content/groups/public"
 )
 
 parallelExecution in Test := false
--- src/main/scala/org/coreyoliver/glacier_cli/Main.scala
+++ src/main/scala/org/coreyoliver/glacier_cli/Main.scala
@@ -1,13 +1,36 @@
-package glacier_cli
+package org.coreyjonoliver.glacier_cli
 
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.auth.BasicAWSCredentials
-import org.rogach.scallop._
-import org.rogach.scallop.exceptions._
+import org.rogach.scallop.{Subcommand, ScallopConf}
 
-object Main extends App {
-  println("Hello")
+object Main {
+
+   def main(args: Array[String]) {
+	   val conf = new ScallopConf(args) {
+	     version("")
+	     banner("")
+	     footer("")
+	     
+	     val createVault = new Subcommand("create-vault") {
+	       val vaultName = trailArg[String]("vault-name", "The vault name.", required = true)
+	     }
+	     
+	     override def onError(e: Throwable) = {
+	       println("Error: %s".format(e.getMessage))
+	       printHelp()
+	       sys.exit(1)
+	     }
+	   }
+	   
+	   conf.subcommand match {
+	     case Some(conf.createVault) => {
+	     }
+	     case None => {}
+	   }
+   }
 }
+
 
 
 
